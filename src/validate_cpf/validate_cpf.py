ef5578089417f2f6265d24e50fe37921e0836207
Drummerzzz <jf.interatividade@gmail.com>
2021-11-14 14:36:24 -0300

    :recycle: refact: Add typing return functions

--- src/validate_cpf/validate_cpf.py
+++ src/validate_cpf/validate_cpf.py
@@ -1,36 +1,43 @@
 import re
+from typing import List
 
-def clean(cpf:str):
-  return ''.join(re.findall('[0-9]+', cpf))
 
-def has_correct_length(cpf:str):
-  CPF_VALID_SIZE = 11
-  return len(cpf) == CPF_VALID_SIZE
+def __clean(cpf: str) -> str:
+    return ''.join(re.findall('[0-9]+', cpf))
 
-def is_allowed(cpf:str):
-  first_digit = cpf[0]
-  return not all(first_digit == digit for digit in cpf)
 
-def generate_factories(factory:int, min_factory:int = 2):
-  return [i for i in reversed(range(min_factory, factory + 1))] # Número magico
+def __has_correct_length(cpf: str) -> bool:
+    CPF_VALID_SIZE = 11
+    return len(cpf) == CPF_VALID_SIZE
 
-def caculate_digit(cpf:str, is_first_digit:bool=True):
-  FACTORY = 10 if is_first_digit else 11
-  FACTORIES = generate_factories(factory=FACTORY)
-  result_sum = 0
-  for index, factory, in enumerate(FACTORIES, 0):
-    result_sum += factory * int(cpf[index])
-  REST = result_sum % 11 # Número magico
-  if REST < 2 or REST > 10:
-    return 0
-  return 11 - REST
 
-def is_valid(cpf:str):
-  cpf = clean(cpf)
-  if not has_correct_length(cpf):
-    return False
-  if not is_allowed(cpf):
-    return False
-  first_verification_digit = caculate_digit(cpf)
-  last_verification_digit = caculate_digit(cpf, is_first_digit=False)
-  return cpf[-2:] == f'{first_verification_digit}{last_verification_digit}'
+def __is_allowed(cpf: str) -> bool:
+    first_digit = cpf[0]
+    return not all(first_digit == digit for digit in cpf)
+
+
+def __generate_factories(factory: int, min_factory: int = 2) -> List[int]:
+    return [i for i in reversed(range(min_factory, factory + 1))]  # Número magico
+
+
+def __caculate_digit(cpf: str, is_first_digit: bool = True) -> bool:
+    FACTORY = 10 if is_first_digit else 11
+    FACTORIES = __generate_factories(factory=FACTORY)
+    result_sum = 0
+    for index, factory in enumerate(FACTORIES, 0):
+        result_sum += factory * int(cpf[index])
+    REST = result_sum % 11  # Número magico
+    if REST < 2 or REST > 10:
+        return 0
+    return 11 - REST
+
+
+def is_valid(cpf: str) -> bool:
+    cpf: str = __clean(cpf)
+    if not __has_correct_length(cpf):
+        return False
+    if not __is_allowed(cpf):
+        return False
+    first_verification_digit: int = __caculate_digit(cpf)
+    last_verification_digit: int = __caculate_digit(cpf, is_first_digit=False)
+    return cpf[-2:] == f'{first_verification_digit}{last_verification_digit}'
